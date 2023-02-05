87458c96e92b800d8b68c903abfb4422c2d4b1b1
Saba Okriashvili <sabaaoqrish@gmail.com>
2023-02-05 14:24:13 +0400

    addded  decrement button

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 let countEl = document.getElementById("count-el");
-let count = 0;
 let saveEl = document.getElementById("save-el");
+let count = 0;
 
 function increment() {
     count += 1
     countEl.innerText = count
 }
 
+function decrement() {
+    count -= 1
+    countEl.innerText = count
+}
+
 function save() {
-    let save = " " + count + " -"
-    saveEl.innerText += save 
+    let savestr = " " + count + " ,"
+    saveEl.innerText += savestr 
     console.log(count);
 }
